c495c94db551dfd4afdf185d0593ebac606c15dc
julio gabriel garcia <julio.garcia.profissional@gmail.com>
2020-10-14 09:51:29 -0300

    iniciando o style com sass no about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,76 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-card">
+    <div class="card-title">
+      <img height="50px" src="https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/43234209_1915143271906341_8651364783385214976_o.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=9Jg6tGs_ML8AX_rRogG&_nc_ht=scontent-gru2-2.xx&oh=a389007bb0b0c6a4f2baeca789e7267c&oe=5FAC1452" 
+        alt="avatar" class="avatar">
+        <div class="title-text-content">
+          <p>About me</p>
+          <h2>Júlio Garcia</h2>
+        </div>
+      </div>
+      <div class="card-content">
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Unde consectetur et repudiandae fugiat in atque repellendus corporis recusandae
+          alias ipsum laborum doloribus labore nesciunt, eum porro,
+          ipsam architecto distinctio eius. Aut commodi odio nulla odit.
+          Repellendus illo quos eum optio facilis architecto velit porro.
+          Maiores sequi reiciendis quo repellendus deleniti?</p>
+
+          <table>
+            <tr>
+              <th>Requirement</th>
+              <th>Solution</th>
+            </tr>
+            <tr>
+              <td>Design</td>
+              <td>Photoshop</td>
+            </tr>
+            <tr>
+              <td>Front-end</td>
+              <td>Vue.js, HTML, CSS, JavaScript</td>
+            </tr>
+            <tr>
+              <td>Back-end</td>
+              <td>Laravel</td>
+            </tr>
+            <tr>
+              <td>Versioning</td>
+              <td>GitHub, GitLab</td>
+            </tr>
+            <tr>
+              <td>Database</td>
+              <td>SqlServer, MySQL, Workbench, PHPMyAdmin, Postman</td>
+            </tr>
+          </table>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Unde consectetur et repudiandae fugiat in atque repellendus corporis recusandae
+          alias ipsum laborum doloribus labore nesciunt, eum porro,
+          ipsam architecto distinctio eius. Aut commodi odio nulla odit.
+          Repellendus illo quos eum optio facilis architecto velit porro.
+          Maiores sequi reiciendis quo repellendus deleniti?</p>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Unde consectetur et repudiandae fugiat in atque repellendus corporis recusandae
+          alias ipsum laborum doloribus labore nesciunt, eum porro,
+          ipsam architecto distinctio eius. Aut commodi odio nulla odit.
+          Repellendus illo quos eum optio facilis architecto velit porro.
+          Maiores sequi reiciendis quo repellendus deleniti?</p>
+
+      </div>
   </div>
 </template>
+<style lang="sass" scoped>
+.about-card 
+  background: #333
+  padding: 3em
+  border-radius: 10px
+  color: white
+.card-title
+  display: flex
+  align-items: center
+  background: #222
+  border-radius: 10px
+
+</style>
